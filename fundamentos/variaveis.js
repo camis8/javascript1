adaa8241c0ad99e7ea787082de70e20113cd60fa
Camila <caahmenezes97@gmail.com>
2024-09-13 21:32:52 -0300

    estudo das variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,24 @@
+/**
+ * Estudo de constante e variáveis
+ * @author Camila
+
+ */
+console.clear()
+
+// variaveis
+let nome 
+let idade 
+let vip 
+
+nome = "Camila"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 56
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
+
