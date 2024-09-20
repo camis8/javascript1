f2fa2684299f0b55f0dd9dfb564581e87f5567ae
Camila <caahmenezes97@gmail.com>
2024-09-19 22:19:21 -0300

    alteração arquivo

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha do aluno
+ * @author Camila
+ * 
+ */
+// variaveis
+let Nome
+let Idade
+let Peso
+let Altura
+let Vip
+
+console.clear()
+
+// entrada de dados
+nome = "Camila"
+idade = 56
+peso = 62
+altura = 1.63
+vip = true
+
+// saida
+console.log("Ficha do aluno")
+console.log("_____________________________________________")
+console.log('Nome: ${nome}')
+console.log('Idade: ${idade}')
+console.log('Altura: ${altura}')
+console.log('Peso: ${altura}')
+console.log('Vip: ${vip}')
