5380646bffeb1ba22452deddc63880c8a0010072
Camila <caahmenezes97@gmail.com>
2024-09-26 22:23:28 -0300

    javascript1

---
+++ fundamentos/biblioteca.js
@@ -0,0 +1,11 @@
+/**
+ * Exemplo de uso de pacotes externos ("bibliotecas")
+ * @author Camila
+ * 
+ */
+// importar o pacote
+const colors = require('colors')
+
+console.clear()
+
+console.log("Hello World".green)
---
+++ fundamentos/input.js
@@ -0,0 +1,14 @@
+/**
+*input de dados via terminal
+* Uso do pacote read-line-sync
+*@author Camila*/
+
+
+// importar o pacote 
+const input =require('readline-sync')
+
+let nome
+console.clear()
+
+nome = input.question("Digite o seu nome:")
+console.log(`Hello ${nome}`)
